583faaadfc19aa315861491fc36d5197e7666fa3
Ash Wolf <ninji@wuffs.org>
2020-04-02 18:11:07 +0100

    fix bcsv dumper

--- dump_all_bcsvs.py
+++ dump_all_bcsvs.py
@@ -1,13 +1,16 @@
-import bcsv, binascii, importlib, json, os, os.path, sys
+import bcsv, binascii, csv, importlib, json, os, os.path, sys
 
 in_path = sys.argv[1]
 out_dir = sys.argv[2]
 specs_file = sys.argv[3]
 specs = importlib.import_module(specs_file.replace('.py', ''))
 
+csv_dir = out_dir + '/csv'
 json_dir = out_dir + '/json'
 html_dir = out_dir + '/html'
 
+if not os.path.exists(csv_dir):
+	os.makedirs(csv_dir)
 if not os.path.exists(json_dir):
 	os.makedirs(json_dir)
 if not os.path.exists(html_dir):
@@ -26,21 +29,28 @@ for filename, row_class in specs.lookup.items():
 	with open('%s/%s' % (in_path, filename), 'rb') as f:
 		b.load(f.read())
 
+	csvw = csv.writer(open('%s/%s.csv' % (csv_dir, filename.replace('.bcsv', '')), 'w'))
+
 	html.append('<table>')
 	html.append('<tr>')
 	for field in row_class.fields():
 		html.append('<th>%s</th>' % field)
+	csvw.writerow(row_class.fields())
 	html.append('</tr>')
 	for row in b.rows:
 		html.append('<tr>')
 		obj = {}
+		crow = []
 		for field in row_class.fields():
 			value = getattr(row, field)
 			html.append('<td>%r</td>' % (value, ))
 			if isinstance(value, bytes):
-				obj[field] = binascii.hexlify(value).decode('ascii')
+				value = binascii.hexlify(value).decode('ascii')
+			crow.append(value)
+			obj[field] = value
 		html.append('</tr>')
 		objs.append(obj)
+		csvw.writerow(crow)
 	html.append('</table>')
 
 	html.append('</body>')
